9d57d7bcec4b14ffcf1ba1ef40ec4dd727f80257
서민주 <minju1307@gmail.com>
2020-10-12 01:44:12 +0900

    키패드 누르기 switch문->딕셔너리 사용

--- 3주차/서민주/KeyPadPressing.swift
+++ 3주차/서민주/KeyPadPressing.swift
@@ -4,35 +4,22 @@ func solution(_ numbers:[Int], _ hand:String) -> String {
     var answer = ""
     var l = [3, 0]
     var r = [3, 2]
+    var keypad = [1: [0, 0], 2: [0, 1], 3: [0, 2],
+                  4: [1, 0], 5: [1, 1], 6: [1, 2],
+                  7: [2, 0], 8: [2, 1], 9: [2, 2],
+                  0: [3, 1]]
 
     for num in numbers {
         if [1, 4, 7].contains(num) {
             answer += "L"
-            switch num {
-            case 1: l = [0, 0]
-            case 4: l = [1, 0]
-            case 7: l = [2, 0]
-            default: break
-            }
+            l = keypad[num]!
             
         } else if [3, 6, 9].contains(num) {
             answer += "R"
-            switch num {
-            case 3: r = [0, 2]
-            case 6: r = [1, 2]
-            case 9: r = [2, 2]
-            default: break
-            }
+            r = keypad[num]!
             
         } else {
-            var coordinate = [Int]()
-            switch num {
-            case 2: coordinate = [0, 1]
-            case 5: coordinate = [1, 1]
-            case 8: coordinate = [2, 1]
-            case 0: coordinate = [3, 1]
-            default: break
-            }
+            var coordinate = keypad[num]!
 
             let l_diff = abs(l[0]-coordinate[0]) + abs(l[1]-coordinate[1])
             let r_diff = abs(r[0]-coordinate[0]) + abs(r[1]-coordinate[1])
