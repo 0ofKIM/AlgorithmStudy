863927fe7fdde16b7f3bbb69e608b13b5b89647a
서민주 <minju1307@gmail.com>
2020-10-12 02:26:03 +0900

    다리위의 트럭 2차원배열->튜플로 변경

--- 3주차/서민주/TrucksPassingBridge.swift
+++ 3주차/서민주/TrucksPassingBridge.swift
@@ -3,26 +3,28 @@ import Foundation
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     var answer = 0
     var cur_weight = 0
-    var onbridge = [[Int]]()
+    var onbridge = [(Int, Int)]()
 
     for (t_idx, truck) in truck_weights.enumerated() {
         cur_weight += truck
-        onbridge.append([truck, bridge_length])
-       
+        onbridge.append((truck, bridge_length))
+
         while true {
             answer += 1
             for b_idx in 0..<onbridge.count {
-                onbridge[b_idx][1] -= 1
+                onbridge[b_idx].1 -= 1
             }
-            if onbridge[0][1] <= 0 {
-                cur_weight -= onbridge[0][0]
+            
+            if onbridge[0].1 <= 0 {
+                cur_weight -= onbridge[0].0
                 onbridge.removeFirst()
                 if onbridge.isEmpty { break }
-            } 
-            if t_idx < truck_weights.count-1 && 
+            }
+            
+            if t_idx < truck_weights.count-1 &&
                 cur_weight + truck_weights[t_idx+1] <= weight { break }
         }
     }
-    
+
     return answer + 1
 }
