b02986d3414291fa32b859fa3b4c4f2b170d3ef0
서민주 <minju1307@gmail.com>
2020-10-12 02:04:58 +0900

    키패드 누르기 좌표 튜플로 변경, if문 switch문으로 대체

--- 3주차/서민주/KeyPadPressing.swift
+++ 3주차/서민주/KeyPadPressing.swift
@@ -2,27 +2,28 @@ import Foundation
 
 func solution(_ numbers:[Int], _ hand:String) -> String {
     var answer = ""
-    var l = [3, 0]
-    var r = [3, 2]
-    var keypad = [1: [0, 0], 2: [0, 1], 3: [0, 2],
-                  4: [1, 0], 5: [1, 1], 6: [1, 2],
-                  7: [2, 0], 8: [2, 1], 9: [2, 2],
-                  0: [3, 1]]
+    var l = (3, 0)
+    var r = (3, 2)
+    let keypad = [1: (0, 0), 2: (0, 1), 3: (0, 2),
+                  4: (1, 0), 5: (1, 1), 6: (1, 2),
+                  7: (2, 0), 8: (2, 1), 9: (2, 2),
+                  0: (3, 1)]
 
     for num in numbers {
-        if [1, 4, 7].contains(num) {
+        switch num {
+        case 1, 4, 7:
             answer += "L"
             l = keypad[num]!
             
-        } else if [3, 6, 9].contains(num) {
+        case 3, 6, 9:
             answer += "R"
             r = keypad[num]!
             
-        } else {
-            var coordinate = keypad[num]!
+        default:
+            let coordinate = keypad[num]!
 
-            let l_diff = abs(l[0]-coordinate[0]) + abs(l[1]-coordinate[1])
-            let r_diff = abs(r[0]-coordinate[0]) + abs(r[1]-coordinate[1])
+            let l_diff = abs(l.0-coordinate.0) + abs(l.1-coordinate.1)
+            let r_diff = abs(r.0-coordinate.0) + abs(r.1-coordinate.1)
             
             if l_diff < r_diff || (l_diff == r_diff && hand == "left"){
                 answer += "L"
