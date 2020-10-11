dc28f3e6cfe065ef3295e3628a2704016870c531
uykim3 <uykim3@lotte.net>
2020-10-11 11:53:29 +0900

    프로그래머스) 다리를 지나는 트럭

---
+++ 3주차/TruckPassingTheBridge.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var answer = 0
+    var timeOnBridge: [Int] = [] //트럭이 다리에 오른 시간을 저장
+    var bridgeWeight = 0
+    var truckOnTheBridge: [Int] = []
+    var waitingTruck = truck_weights
+
+    while true {
+        answer += 1
+
+        if let truck = truckOnTheBridge.first, (answer-timeOnBridge.first!) == bridge_length {
+            truckOnTheBridge.removeFirst()
+            timeOnBridge.removeFirst()
+            bridgeWeight -= truck
+        }
+
+        if let truck = waitingTruck.first, (bridgeWeight + truck) <= weight {
+            truckOnTheBridge.append(truck)
+            timeOnBridge.append(answer)
+            bridgeWeight += truck
+            waitingTruck.removeFirst()
+        }
+
+        //print("경과시간: \(answer), 다리를 건너는 트럭: \(truckOnTheBridge), 대기 트럭: \(waitingTruck)")
+        if truckOnTheBridge.isEmpty && waitingTruck.isEmpty {
+            return answer
+        }
+    }
+
+    return answer
+}
