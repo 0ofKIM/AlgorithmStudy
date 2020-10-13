0789769f8d4cef99a52bee9807e45f5d998b9097
uykim3 <uykim3@lotte.net>
2020-10-13 16:20:30 +0900

    프로그래머스 - 기능개발

---
+++ 4주차/김의영/FunctionDevelopment.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    var days: [Int] = []
+    var day: Double = 0
+
+    progresses.enumerated().map { (index, progress) in
+        if day < ceil(Double(100-progress) / Double(speeds[index])) {
+            day = ceil(Double(100-progress) / Double(speeds[index]))
+            days.append(1)
+        } else {
+            days[days.count-1] += 1
+        }
+    }
+
+    return days
+}
