20f7b54a82bf243584c80edaf1d053101cecf806
uykim3 <uykim3@lotte.net>
2020-10-18 23:24:58 +0900

    프로그래머스 - 괄호변환(level2, 카카오 블라인드 채용)

---
+++ 4주차/김의영/ChangeParenthesis.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ p:String) -> String {
+    guard !p.isEmpty else { return "" }
+
+    var u = ""
+    var v = ""
+    var correct = 0
+
+    for (index, text) in p.enumerated() {
+        u.append(text)
+        correct += (text == "(") ? 1 : -1
+        if correct == 0 && index+1 != p.count {
+            v = String(p[p.index(p.startIndex, offsetBy: index+1)...])
+        }
+
+        if (u.count%2 == 0) && (correct == 0) {
+            if u.last == ")" {
+                u.append(solution(v))
+
+                return u
+            } else {
+                u.removeFirst()
+                u.removeLast()
+
+                return "(" + solution(v) + ")" + u.map { $0 == "(" ? ")" : "(" }
+            }
+        }
+    }
+
+    return ""
+}
