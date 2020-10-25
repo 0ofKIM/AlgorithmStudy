7438dcfd1efdb17f96571735e5144f5c1182f675
서민주 <minju1307@gmail.com>
2020-10-26 02:18:36 +0900

    불필요한 딕셔너리 삭제 및 filter대신 contains 사용

--- 5주차/서민주/Printer.swift
+++ 5주차/서민주/Printer.swift
@@ -4,12 +4,11 @@ func solution(_ priorities:[Int], _ location:Int) -> Int {
     var answer = 0
     var currentLocation = location
     var priorityQueue = priorities
-    var priorityDict = priorities.reduce(into: [:]) { counts, word in counts[word, default: 0] += 1 }
     
     while !priorityQueue.isEmpty {
         let doc = priorityQueue[0]
         
-        if priorityDict.keys.filter({ $0 > doc }).count > 0 {
+        if priorityQueue.contains(where: { $0 > doc }) {
             priorityQueue.removeFirst()
             priorityQueue.append(doc)
             if currentLocation == 0 {
@@ -18,8 +17,6 @@ func solution(_ priorities:[Int], _ location:Int) -> Int {
         } else {
             answer += 1
             priorityQueue.removeFirst()
-            if priorityDict[doc] == 1 { priorityDict[doc] = nil }
-            else { priorityDict[doc]! -= 1 }
             if currentLocation == 0 { return answer }
         }
         currentLocation -= 1
