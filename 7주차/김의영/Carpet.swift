962572ff2b350081418371f779b8af66c6385164
0ofKim <52947210+0ofKIM@users.noreply.github.com>
2020-11-12 10:52:53 +0900

    Update Carpet.swift

--- 7주차/김의영/Carpet.swift
+++ 7주차/김의영/Carpet.swift
@@ -4,11 +4,7 @@ func solution(_ brown:Int, _ yellow:Int) -> [Int] {
     var height = 3
     var width = (brown+yellow)/height
     
-    while true {
-        if (width-2) * (height-2) == yellow {
-            break
-        }
-        
+    while (width-2) * (height-2) != yellow {
         height += 1
         width = (brown+yellow)/height
     }
