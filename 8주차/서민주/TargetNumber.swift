aa18509d1aab4deaeecbefc2ec4d65b7df587f92
서민주 <minju1307@gmail.com>
2020-11-15 17:32:59 +0900

    타겟넘버 변수 삭제

--- 8주차/서민주/TargetNumber.swift
+++ 8주차/서민주/TargetNumber.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    var len = numbers.count
+
     func dfs(_ sum: Int, _ index: Int) -> Int {
-        if index == len { return sum == target ? 1 : 0 }
+        if index == numbers.count { return sum == target ? 1 : 0 }
         return dfs(sum-numbers[index], index+1) + dfs(sum+numbers[index], index+1)
     }
     
