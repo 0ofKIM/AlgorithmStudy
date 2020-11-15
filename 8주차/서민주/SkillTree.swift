d1077b8f5656bb8ebf81fd605f1b7f383a2d90a2
서민주 <minju1307@gmail.com>
2020-11-15 17:28:27 +0900

    스킬트리 result 초기값 변경

--- 8주차/서민주/SkillTree.swift
+++ 8주차/서민주/SkillTree.swift
@@ -1,9 +1,7 @@
 func solution(_ skill:String, _ skill_trees:[String]) -> Int {
-    var result = 0
+    var result = skill_trees.count
     
     for skillTree in skill_trees {
-        result += 1
-        
         var index = skill.startIndex
         for char in skillTree {
             if skill.contains(char) {
