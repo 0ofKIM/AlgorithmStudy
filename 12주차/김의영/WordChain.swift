09e8941519c646732c52e71d6e6fa21a5038d282
0ofKim <52947210+0ofKIM@users.noreply.github.com>
2020-12-14 13:53:30 +0900

    Update WordChain.swift
    복잡했던 return 값 수정

--- 12주차/김의영/WordChain.swift
+++ 12주차/김의영/WordChain.swift
@@ -5,7 +5,7 @@ func solution(_ n:Int, _ words:[String]) -> [Int] {
     var endChar: Character = " "
     for (index, word) in words.enumerated() {
         if (index > 0 && endChar != word.first) || usedWords.contains(word) {
-            return [(index+1)%n == 0 ? n : (index+1)%n, (index+1)%n == 0 ? (index+1)/n : (index+1)/n + 1]
+            return [(index%n)+1, (index/n)+1]
         }
 
         endChar = word.last!
