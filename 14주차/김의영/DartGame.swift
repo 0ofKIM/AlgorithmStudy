0907e96b61f92650cbdd1359a8887db90ce76fe8
uykim3 <uykim3@lotte.net>
2020-12-26 16:29:12 +0900

    프로그래머스 - 다트게임(level1, 2018 KAKAO BLIND)

---
+++ 14주차/김의영/DartGame.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func solution(_ dartResult:String) -> Int {
+    var score: [Int] = []
+
+    for dart in dartResult {
+        let index = score.count > 0 ? score.count-1 : 0
+
+        switch String(dart) {
+        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+            if dart == "0" && score.count > 0 {
+                if score[index] == 1 { score[index] = 10 }
+                else { score.append(Int(String(dart))!) }
+            } else {
+                score.append(Int(String(dart))!)
+            }
+        case "*":
+            score[index] = score[index] * 2
+            if score.count > 1 { score[index-1] = score[index-1] * 2 }
+        case "#":
+            score[index] = score[index] * (-1)
+        case "D", "T":
+            score[index] = dart == "D" ? Int(pow(Double(score[index]), 2.0)) : Int(pow(Double(score[index]), 3.0))
+        default: break
+        }
+    }
+
+    return score.reduce(0) { $0 + $1 }
+}
