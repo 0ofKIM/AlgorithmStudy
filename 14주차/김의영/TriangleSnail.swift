4d72585865a511653d358c653c421311d8ec4d0d
0ofKim <52947210+0ofKIM@users.noreply.github.com>
2020-12-30 15:44:16 +0900

    Update TriangleSnail.swift
    리뷰 후 수정

--- 14주차/김의영/TriangleSnail.swift
+++ 14주차/김의영/TriangleSnail.swift
@@ -1,14 +1,18 @@
 import Foundation
 
-enum direction {
+enum Direction: Int {
     case down
     case straight
     case up
+
+    mutating func next() {
+        self = Direction(rawValue: rawValue + 1)!
+    }
 }
 
 func solution(_ n:Int) -> [Int] {
     var result = Array(repeating: [Int](), count: n)
-    var direction: direction = .down // 진행방향
+    var direction: Direction = .down // 진행방향
     var value: Int = 1 // 숫자 값
     var row: Int = 0 // 행
     var countInStroke: Int = n // 한 획에 그릴 갯수
@@ -20,19 +24,17 @@ func solution(_ n:Int) -> [Int] {
             case .down:
                 result[row].insert(value, at: thickness)
                 row += snailCount==countInStroke ? 0 : 1
-                if snailCount==countInStroke { direction = .straight }
             case .straight:
                 result[row].insert(value, at: result[row].count-thickness)
                 row -= snailCount==countInStroke ? 1 : 0
-                if snailCount==countInStroke { direction = .up }
             case .up:
                 result[row].insert(value, at: result[row].count-thickness)
                 row -= snailCount==countInStroke ? -1 : 1
-                if snailCount==countInStroke { direction = .down }
             }
             value += 1
         }
 
+        direction.next()
         if stroke % 3 == 0 { thickness += 1 }
         countInStroke -= 1
     }
