663a5deb5a995c9f06dfb43a0bf2e3bb34b1164b
서민주 <minju1307@gmail.com>
2021-01-12 17:31:42 +0900

    문자열 압축 리팩토링

--- 16주차/서민주/StringCompression.swift
+++ 16주차/서민주/StringCompression.swift
@@ -1,45 +1,35 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    
     guard s.count > 2 else { return s.count }
-    
     var sArr = Array(s)
     var min = s.count
     
     func compress(_ tokenLength: Int) -> Int {
         var words = [ArraySlice<Character>]()
-
         for i in stride(from: 0, through: s.count-tokenLength, by: tokenLength) {
-            words.append(sArr[i..<i+tokenLength])    
+            words.append(sArr[i..<i+tokenLength])
         }
-        words += [ArraySlice<Character>()]
+        words.append(ArraySlice<Character>())
         
-        var compressedLength = 0
-        compressedLength += s.count % tokenLength        
-
+        var compressedLength = s.count % tokenLength
         var (word, cnt) = (words[0], 1)
-        
         for (front, back) in zip(words, words[1...]) {
             if front == back {
                 cnt += 1
-                
-            } else {
+            }
+            else {
                 compressedLength += (cnt == 1 ? 0 : cnt < 10 ? 1 : 2) + word.count
-                word = back
-                cnt = 1
+                (word, cnt) = (back, 1)
             }
         }
-
         return compressedLength
     }
     
     for tokenLength in (1...s.count/2).reversed() {
         let res = compress(tokenLength)
-        
         if res < min { min = res }
     }
-
+    
     return min
 }
-
