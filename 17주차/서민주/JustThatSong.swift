457f67306f88f5589535259fef5f2d913904aa2d
서민주 <minju1307@gmail.com>
2021-01-21 20:51:09 +0900

    방금그곡 리팩토링

--- 17주차/서민주/JustThatSong.swift
+++ 17주차/서민주/JustThatSong.swift
@@ -1,29 +1,32 @@
 func solution(_ m:String, _ musicinfos:[String]) -> String {
+    
+    func sharpConverter(_ note: String) -> String {
+        var converted = note
+        for (sharp, lower) in zip(["C#", "D#", "F#", "G#", "A#"], ["c", "d", "f", "g", "a"]) {
+            converted = converted.replacingOccurrences(of: sharp, with: lower)
+        }
+        return converted
+    }
+    
     var result = (title: "", duration: 0)
-    var mConverted = m
+    var mConverted = sharpConverter(m)
     
-    for info in musicinfos {    
-        let splitted = info.components(separatedBy: ",")    
-    
+    for info in musicinfos {
+        let splitted = info.components(separatedBy: ",")
         let (startTimeStr, endTimeStr, title) = (splitted[0], splitted[1], splitted[2])
-        var notes = splitted[3]
+        var notes = sharpConverter(splitted[3])
         
         let startTime = startTimeStr.components(separatedBy: ":").map{ Int($0)! }
         let endTime = endTimeStr.components(separatedBy: ":").map{ Int($0)! }
         let duration = (endTime[0] - startTime[0]) * 60 + endTime[1] - startTime[1]
- 
-        for (sharp, lower) in zip(["C#", "D#", "F#", "G#", "A#"], ["c", "d", "f", "g", "a"]) {
-            notes = notes.replacingOccurrences(of: sharp, with: lower)
-            mConverted = mConverted.replacingOccurrences(of: sharp, with: lower)
-        }
         
         var allNotes = String(repeating: notes, count: duration / notes.count)
         allNotes += notes.prefix(duration % notes.count)
-        
+
         if allNotes.contains(mConverted) && result.duration < duration {
             result = (title, duration)
         }
     }
-    
+
     return result.duration == 0 ? "(None)" : result.title
 }
