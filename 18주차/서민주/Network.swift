96bc888d370810623b0448a5407c95790319d1ff
서민주 <minju1307@gmail.com>
2021-01-27 01:01:15 +0900

    네트워크 리팩토링

--- 18주차/서민주/Network.swift
+++ 18주차/서민주/Network.swift
@@ -4,21 +4,17 @@ func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     var cnt = 0
     var connected = Array(repeating: false, count: n)
     
-    func bfs(_ i: Int) {
+    func dfs(_ i: Int) {
         connected[i] = true
         
-        for j in 0..<n { 
-            if computers[i][j] == 1 && !connected[j] { 
-                bfs(j)
-            }
+        for j in 0..<n where computers[i][j] == 1 && !connected[j] {
+            dfs(j)
         }
     }
     
-    (0..<n).forEach {
-        if !connected[$0] {
-            cnt += 1
-            bfs($0)    
-        }
+    for i in 0..<n where !connected[i] {
+        cnt += 1
+        dfs(i)
     }
     
     return cnt
