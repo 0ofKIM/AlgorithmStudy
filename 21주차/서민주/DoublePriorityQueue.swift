0837d0c180742b664a78ff2ce9a01570535bf5bc
서민주 <minju1307@gmail.com>
2021-02-15 04:57:44 +0900

    이중우선순위큐 리팩토링

--- 21주차/서민주/DoublePriorityQueue.swift
+++ 21주차/서민주/DoublePriorityQueue.swift
@@ -2,22 +2,14 @@ import Foundation
 
 func solution(_ operations:[String]) -> [Int] {
     var q = [Int]()
-    var (min, max) = (0, 0)
     
     for op in operations {
-        let separated = op.components(separatedBy: " ")
-        let (command, num) = (separated[0], Int(separated[1])!)
-        
-        if command == "I" {
-            q.append(num)
-        } else if num == 1 {
-            if let maxNum = q.max(), let maxIdx = q.index(of: maxNum) {
-                q.remove(at: maxIdx)    
-            }
-        } else if num == -1 {
-            if let minNum = q.min(), let minIdx = q.index(of: minNum) {
-                q.remove(at: minIdx)    
-            }
+        if op.hasPrefix("I") {
+            let numIdx = op.index(op.startIndex, offsetBy: 2)
+            q.append(Int(op[numIdx...])!)
+            
+        } else if let num = op == "D 1" ? q.max() : q.min(), let idx = q.index(of: num) {
+            q.remove(at: idx)
         }
     }
     
