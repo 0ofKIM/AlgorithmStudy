1cd5a2f8e96ef745030d14f95a363d72fdf87fdc
0ofKim <52947210+0ofKIM@users.noreply.github.com>
2021-03-04 23:52:33 +0900

    Update WordConversion.swift
    1. 반복문 내 if문, where절로 이동
    2. 오타 수정

--- 23주차/김의영/WordConversion.swift
+++ 23주차/김의영/WordConversion.swift
@@ -3,18 +3,16 @@ func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
 
     var isCheck = Array(repeating: false, count: words.count)
     var result = Int.max
-    func dfs(_ start: String, _ changCount: Int) {
+    func dfs(_ start: String, _ changeCount: Int) {
         if start == target {
-            result = min(result, changCount)
+            result = min(result, changeCount)
             return
         }
 
-        for (index, word) in words.enumerated() {
-            if !isCheck[index] && zip(start,word).filter({ $0 != $1 }).count == 1  {
-                isCheck[index] = true
-                dfs(word, changCount+1)
-                isCheck[index] = false
-            }
+        for (index, word) in words.enumerated() where !isCheck[index] && zip(start,word).filter({ $0 != $1 }).count == 1 {
+            isCheck[index] = true
+            dfs(word, changCount+1)
+            isCheck[index] = false
         }
     }
 
