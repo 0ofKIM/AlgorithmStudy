fa819bf4203783450d6089f3e633ab09e972e0a6
서민주 <minju1307@gmail.com>
2021-03-23 17:13:37 +0900

    배달

--- 24주차/서민주/Delivery.swift
+++ 24주차/서민주/Delivery.swift
@@ -1,35 +1,34 @@
 import Foundation
 
 func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
-    let MAX = 500001
-    var answer = [MAX, 0] + Array(repeating: MAX, count: N-1)
-    var map = [Int: [Int]]()
-    var distMap = Array(repeating: Array(repeating: MAX, count: N+1), count: N+1)
+    let MAX = Int.max
     
+    var distances = [MAX, 0] + Array(repeating: MAX, count: N-1)
+    var map: [Int: [Int]] = Dictionary(uniqueKeysWithValues: (1...N).map { ($0, [Int]()) })
+    var distMap = Array(repeating: Array(repeating: MAX, count: N+1), count: N+1)
+
     for r in road {
         let (a, b, dist) = (r[0], r[1], r[2])
-        if !map.keys.contains(a) { map[a] = [Int]() } 
-        if !map.keys.contains(b) { map[b] = [Int]() }
-        map[a]!.append(b) 
+        map[a]!.append(b)
         map[b]!.append(a)
         if distMap[a][b] > dist {
-            distMap[a][b] = dist 
+            distMap[a][b] = dist
             distMap[b][a] = dist
         }
     }
-    
+
     var queue = [1]
     while !queue.isEmpty {
         let start = queue.removeFirst()
-
         for end in map[start]! {
-            let dist = answer[start] + distMap[start][end]
-            if dist < answer[end] && dist <= k {
-                answer[end] = dist	
-                queue.append(end)   
+            let dist = distances[start] + distMap[start][end]
+            
+            if dist < distances[end] && dist <= k {
+                distances[end] = dist
+                queue.append(end)
             }
         }
     }
-        
-    return answer.count - answer.filter{ $0 == MAX }.count
+
+    return distances.filter{ $0 <= k }.count
 }
