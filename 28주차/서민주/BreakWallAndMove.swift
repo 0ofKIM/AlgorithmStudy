ddf570c7bd103de9447d9a06acd3e3e2d1b5cd9f
서민주 <minju1307@gmail.com>
2021-04-26 12:02:02 +0900

    BOJ 2206 벽 부수고 이동하기 - 수정

--- 28주차/서민주/BreakWallAndMove.swift
+++ 28주차/서민주/BreakWallAndMove.swift
@@ -1,37 +1,42 @@
 let NM = readLine()!.split(separator: " ").map({ Int(String($0))! })
 let (N, M) = (NM[0], NM[1])
-let map = (0..<N).map({ _ in readLine()!.map({ String($0) == "1" }) })
 
 let dx = [1, 0, -1, 0]
 let dy = [0, 1, 0, -1]
 
-var visit = Array(repeating: Array(repeating: [0, 0], count: M), count: N)
-var q = [(x: 0, y: 0, didBreak: 0)]
-var result = -1
+var map = (0..<N).map({ _ in readLine()!.map({ (Int(String($0)), false) }) })
+var q = [(x: 0, y: 0, broken: false)]
 
-visit[0][0][0] = 1
+map[0][0] = (2, false)
+var dist = 0
+var idx = 0
+var success = false
 
-while !q.isEmpty {
-    let (x, y, didBreak) = q.removeFirst()
-
-    if (x, y) == (N-1, M-1) {
-        result = visit[x][y][didBreak]
-        break
-    }
-    
-    for i in 0..<4 {
-        let (nx, ny) = (x + dx[i], y + dy[i])
-        guard 0..<N ~= nx && 0..<M ~= ny && visit[nx][ny][didBreak] == 0 else { continue }
-
-        if !map[nx][ny] {
-            q.append((nx, ny, didBreak))
-            visit[nx][ny][didBreak] = visit[x][y][didBreak]+1
-        } else if didBreak == 0 {
-            q.append((nx, ny, 1))
-            visit[nx][ny][0] = visit[x][y][0] + 1
-            visit[nx][ny][1] = visit[nx][ny][0]
+loop: while idx < q.count {
+    dist += 1
+    for i in idx..<q.count {
+        let node = q[i]
+        if node.x == N-1 && node.y == M-1 {
+            success = true
+            break loop
         }
-    }   
+        for i in 0...3 {
+            let (nx, ny) = (node.x + dx[i], node.y + dy[i])
+            guard 0..<N ~= nx && 0..<M ~= ny else { continue }
+            
+            let (visited, brokenSpace) = map[nx][ny]
+            if visited == 0 {
+                map[nx][ny] = (2, node.broken)
+                q.append((nx, ny, node.broken))
+            } else if visited == 1 && !node.broken {
+                map[nx][ny] = (-1, true)
+                q.append((nx, ny, true))
+            } else if visited == 2 && !node.broken && brokenSpace {
+                map[nx][ny] = (2, false)
+                q.append((nx, ny, false))
+            }
+        }
+        idx += 1
+    }
 }
-
-print(result)
+print(success ? dist : -1)
