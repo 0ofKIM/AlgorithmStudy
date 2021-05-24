cfa344a7329a1c77632e30b4badb1aa4647a734b
SEOMINJUUU <minju1307@gmail.com>
2021-05-24 16:43:40 +0900

    백준 - 영화감독 숌(1436), contain666 메서드 개선

--- 33주차/서민주/FilmDirectorShum.swift
+++ 33주차/서민주/FilmDirectorShum.swift
@@ -1,24 +1,19 @@
-import Foundation
+func contains666(_ num: Int) -> Bool {
+    var tmpNum = num
 
-func contains666(_ string: String) -> Bool {
-    var str = string.map{ $0 }
-    while let index = str.firstIndex(of: "6") {
-        if index < str.count-2 && str[index+1] == "6" && str[index+2] == "6" {
-            return true
-        }
-        str = Array(str[(index+1)...])
+    while tmpNum >= 666 {
+        if (tmpNum - 666) % 1000 == 0 { return true }
+        tmpNum /= 10
     }
     return false
 }
 
 if let N = Int(readLine()!) {
-    var count = 0
+    var (num, count) = (666, 1)
 
-    for i in 666...2666799 where contains666(String(i)) {
-        count += 1 
-        if count == N { 
-            print(i)
-            break 
-        }
+    while count < N {
+        num += 1
+        if contains666(num) { count += 1 }
     }
+    print(num)
 }
