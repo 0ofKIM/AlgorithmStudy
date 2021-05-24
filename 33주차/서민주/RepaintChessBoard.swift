f63754e2a6fedc312b79b59a71e75e5c0010647c
SEOMINJUUU <minju1307@gmail.com>
2021-05-24 17:19:02 +0900

    백준 - 체스판 다시 칠하기(1018)

--- 33주차/서민주/RepaintChessBoard.swift
+++ 33주차/서민주/RepaintChessBoard.swift
@@ -19,25 +19,4 @@ for i in 0...(N-8) {
         if repaint < min { min = repaint}
     }
 }
-print(min)let NM = readLine()!.split(separator: " ").map{ Int(String($0))! }
-let (N, M) = (NM[0], NM[1])
-let board = (0..<N).map{ _ in readLine()!.map{ String($0) }}
-
-func shouldRepaint( _ i: Int, _ j: Int) -> Bool {
-    return board[i][j] != ((i + j) % 2 == 0 ? "B" : "W" )
-}
-
-var min = 64
-for i in 0...(N-8) {
-    for j in 0...(M-8) {
-        var repaint = 0
-        for ii in i..<i+8 { 
-            for jj in j..<j+8 where shouldRepaint(ii, jj) { 
-                repaint += 1
-            }
-        }
-        if repaint > 32 { repaint = 64 - repaint }
-        if repaint < min { min = repaint}
-    }
-}
 print(min)
