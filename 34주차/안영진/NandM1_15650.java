cebbc49fedc7f2e73cf033dd4e9f0a22b0e9e62d
hellozin <paul26375@gmail.com>
2021-05-30 20:59:38 +0900

    백준 - N과M (2) (15650)

---
+++ 34주차/안영진/NandM1_15650.java
@@ -0,0 +1,35 @@
+package me.hellozin.ps.baekjoon;
+
+import java.util.Scanner;
+
+public class NandM1_15649 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        int[] used = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            dfs(i, N, M, used, "");
+        }
+    }
+
+    private static void dfs(int num, int count, int depth, int[] used, String sequence) {
+        sequence = sequence + num + " ";
+        depth--;
+
+        if (depth == 0) {
+            // print sequence
+            System.out.println(sequence);
+        } else {
+            used[num] = 1;
+            for (int i = num; i <= count; i++) {
+                if (used[i] == 0) {
+                    dfs(i, count, depth, used, sequence);
+                }
+            }
+        }
+        used[num] = 0;
+    }
+}
