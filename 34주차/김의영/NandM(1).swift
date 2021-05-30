33beb551aeadcd12245f1018b99c35f574f37796
0ofKim <>
2021-05-31 04:17:15 +0900

    백준 - N과M(1) (15649)

---
+++ 34주차/김의영/NandM(1).swift
@@ -0,0 +1,21 @@
+let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (n,m) = (nm[0], nm[1])
+
+func solution(_ numberArray: [Int]) {
+    guard numberArray.count < m else {
+        var numberArrayString = ""
+        numberArray.enumerated().forEach { (index, number) in
+            numberArrayString.append(index == 0 ? "\(number)" : " \(number)" )
+        }
+        print(numberArrayString)
+        return
+    }
+    
+    for i in 1...n where !numberArray.contains(i) {
+        solution(numberArray+[i])
+    }
+}
+
+for i in 1...n {
+    solution([i])
+}
