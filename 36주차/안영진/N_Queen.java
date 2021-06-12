b1539795af40f7da33c031ef771fb360bf2b7f72
hellozin <paul26375@gmail.com>
2021-06-12 15:40:18 +0900

    백준 - N과M (4) (9663), 이차원 배열 대신 일차원 배열 사용하도록 변경

--- 36주차/안영진/N_Queen.java
+++ 36주차/안영진/N_Queen.java
@@ -7,47 +7,49 @@ import java.util.Scanner;
 public class N_Queen {
 
     static int N = 0;
-    static int[][] board;
+    static int[] board;
     static int matchCase = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
-        board = new int[N][N];
+        board = new int[N];
 
         dfs(0);
         System.out.println(matchCase);
     }
 
     private static void dfs(int row) {
+        boolean isSafe;
+        // 현재 row에서 퀸을 놓을 수 있는 col을 탐색
         for (int col = 0; col < N; col++) {
-            // board 값이 0 이상이면 퀸에게 공격받고 있는 칸
-            if (board[row][col] > 0) {
-                continue;
+            isSafe = true;
+            // 현재 col이 이미 놓여진 퀸들에게 공격받는지 확인
+            for (int queenRow = 0; queenRow < row; queenRow++) {
+                // 현재 col이 공격받고 있으면 다음 col로 break
+                int queenCol = board[queenRow];
+                isSafe = isAttackedByQueen(row, col, queenRow, queenCol);
+                if (!isSafe) {
+                    break;
+                }
             }
-
-            // 퀸을 놓을 수 있는 칸이 있고 마지막 퀸인 경우
-            if (row == N - 1) {
-                matchCase++;
-                continue;
-            }
-
-            // 퀸을 row, col 위치에 놓는다
-            moveQueen(row, col, 1);
-            dfs(row + 1);
-            // 퀸을 row, col 위치에서 제거한다
-            moveQueen(row, col, -1);
-        }
-    }
-
-    private static void moveQueen(int i, int j, int flag) {
-        // 퀸에게 공격받는 칸의 값을 +1 한다
-        for (int k = 0; k < N; k++) {
-            for (int l = 0; l < N; l++) {
-                if (k == i || l == j || k - i == l - j || k - i == j - l) {
-                    board[k][l] += flag;
+            // 퀸을 row, col에 놓을 수 있으면,
+            if (isSafe) {
+                if (row == N - 1) {
+                    // 그게 마지막 퀸이면 정답 + 1
+                    matchCase++;
+                } else {
+                    // 보드에 퀸을 놓고 다음 row로 이동
+                    board[row] = col;
+                    dfs(row + 1);
                 }
             }
         }
+    }
+
+    private static boolean isAttackedByQueen(int currentRow, int currentCol, int queenRow, int queenCol) {
+        return !(queenCol == currentCol                                  // 퀸과 같은 열
+                || queenCol - (currentRow - queenRow) == currentCol     // 퀸의 왼쪽 대각선
+                || queenCol + (currentRow - queenRow) == currentCol);   // 퀸의 오른쪽 대각선
     }
 }
